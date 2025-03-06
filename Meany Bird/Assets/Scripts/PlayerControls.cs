1126c6c37b317c555e1d5f2608476765f312d919
MasterHacker10 <quinnsidell@icloud.com>
2025-03-06 15:55:03 -0500

    fgawegwrg

--- Meany Bird/Assets/Scripts/PlayerControls.cs
+++ Meany Bird/Assets/Scripts/PlayerControls.cs
@@ -27,5 +27,12 @@ public class PlayerControls : MonoBehaviour
             rb.velocity = Vector2.up * velocity;
         }
 
-    }
+        private void OnCollisionEnter2D(Collision2D collision) 
+        {
+            if(collision.gameObject.tag === "HighSpike" || collision.gameObject.tag == "LowSpike" || collision.gameObject.tag == "Ground") 
+            {
+
+            }
+        }
+}
 }
