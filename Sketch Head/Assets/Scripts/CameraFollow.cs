6632cfa8531b4220113e55720b38c2d76e4c0310
MasterHacker10 <dennislittlecat@outlook.com>
2025-03-25 16:32:52 -0400

    eqiudvyyrw87fedbvhj

---
+++ Sketch Head/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header ("Target Object")]
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target.position.y > transform.position.y) 
+        {
+            transform.position = new Vector3(0, target.position.y, -10f);
+        }
+    }
+}
